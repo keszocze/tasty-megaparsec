c8d4d5c2745be85bc3c7d76e38a62261d7d4f226
Oliver Keszöcze <oliver.keszoecze@fau.de>
2023-08-03 16:11:19 +0200

    initial running version

---
+++ src/Test/Tasty/Megaparsec.hs
@@ -0,0 +1,448 @@
+module Test.Tasty.Megaparsec (
+    shouldSucceedFor,
+    shouldSucceedOn,
+    shouldFailFor,
+    shouldFailOn,
+    parseSatisfies,
+    parseSatisfies',
+    shouldParse,
+    shouldParse',
+    shouldParseLeaving,
+    shouldParseLeaving',
+    initialState,
+    initialPosState,
+    succeedsLeaving,
+    succeedsLeaving',
+    failsLeaving,
+    failsLeaving',
+)
+where
+
+import Control.Monad (unless, void)
+
+import Test.Tasty.HUnit
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Text.Megaparsec.Error.Builder
+
+{- | Create Assertion to check that the parser parses the given input.
+
+Usage:
+
+> (char 'c') `shouldSucceedFor` "c"
+
+This is a convenience function wrapping 'shouldSucceedOn'.
+-}
+shouldSucceedFor ::
+    (VisualStream s, TraversableStream s, ShowErrorComponent e, HasCallStack) =>
+    -- | The 'Parsec' parser to run
+    Parsec e s a ->
+    -- | The input to run the parser on
+    s ->
+    Assertion
+p `shouldSucceedFor` i = parse p "" `shouldSucceedOn` i
+
+{- | Create Assertion to check that a parser parses the given input.
+
+This is a more general version of 'shouldSucceedFor' that expects a parser
+from a stream to either an error or parsing result (instead of a 'Parsec') as its first input.
+
+Usage:
+
+> parse (char 'c') "" `shouldSucceedOn` "c"
+-}
+shouldSucceedOn ::
+    (VisualStream s, TraversableStream s, ShowErrorComponent e, HasCallStack) =>
+    -- | The parser to run
+    (s -> Either (ParseErrorBundle s e) b) ->
+    -- | The input to run the parser on
+    s ->
+    Assertion
+p `shouldSucceedOn` i = expectSuccess_ $ p i
+
+{- | Create Assertion to check that a parser produces the specified result.
+
+Usage:
+
+> shouldParse' (char 'c') "c" 'c'
+
+It is a convenience function wrapping 'shouldParse'.
+-}
+shouldParse' ::
+    ( Show a
+    , VisualStream s
+    , TraversableStream s
+    , ShowErrorComponent e
+    , Eq a
+    , HasCallStack
+    ) =>
+    -- | The 'Parsec' parser to run
+    Parsec e s a ->
+    -- | The input to run the parser on
+    s ->
+    -- | The expected parsing result
+    a ->
+    Assertion
+shouldParse' p i o = parse p "" i `shouldParse` o
+
+{- | Create Assertion to check that a parser produced the specified result.
+
+
+This is a more general version of 'shouldParse'' that expects either an error or
+a parsing result (instead of a 'Parsec') as its first input.
+
+Usage:
+
+> parse (char 'c') "" "c" `shouldParse` 'c'
+-}
+shouldParse ::
+    (Show a, VisualStream s, TraversableStream s, ShowErrorComponent e, Eq a, HasCallStack) =>
+    -- | The result parsing, e.g. via 'parse'
+    Either (ParseErrorBundle s e) a ->
+    -- | The expected parsing result
+    a ->
+    Assertion
+r `shouldParse` v = case r of
+    Left e ->
+        assertFailure $
+            "expected: "
+                ++ show v
+                ++ "\nbut parsing failed with error:\n"
+                ++ showBundle e
+    Right x -> v @=? x
+
+{- | Create an Assertion by saying that the parser should successfully
+parse a value and that the value should satisfy some predicate.
+
+> parse (many alphaNumChar) "" "abc" `parseSatisfies` ((== 3) . length)
+-}
+parseSatisfies ::
+    ( VisualStream s
+    , TraversableStream s
+    , ShowErrorComponent e
+    , Show t
+    ) =>
+    -- | Result of parsing as returned by function like 'parse'
+    Either (ParseErrorBundle s e) t ->
+    -- | Predicate
+    (t -> Bool) ->
+    IO ()
+r `parseSatisfies` p = case r of
+    Left e ->
+        assertFailure $
+            "expected a parsed value to check against the predicate"
+                ++ "\nbut parsing failed with error:\n"
+                ++ showBundle e
+    Right x ->
+        unless (p x) . assertFailure $
+            "the value did not satisfy the predicate: " ++ show x
+
+{- | Create an Assertion by saying that the parser should successfully
+parse a value and that the value should satisfy some predicate.
+
+This function is a convenience function wrapping 'parseSatisfies'.
+
+Usage:
+
+> parseSatisfies' (many alphaNumChar) "abc!nothere!" ((== 3) . length)
+-}
+parseSatisfies' ::
+    ( VisualStream s
+    , TraversableStream s
+    , ShowErrorComponent e
+    , Show a
+    , HasCallStack
+    ) =>
+    Parsec e s a ->
+    s ->
+    (a -> Bool) ->
+    IO ()
+parseSatisfies' p i o = parse p "" i `parseSatisfies` o
+
+{- | Render a parse error bundle in a way that is suitable for inserting it
+in a test suite report.
+-}
+showBundle ::
+    ( VisualStream s
+    , TraversableStream s
+    , ShowErrorComponent e
+    , HasCallStack
+    ) =>
+    ParseErrorBundle s e ->
+    String
+showBundle = unlines . fmap indent . lines . errorBundlePretty
+  where
+    indent x =
+        if null x
+            then x
+            else "  " ++ x
+
+{- | Create Assertion to check that a parser fails to parse the given input.
+
+Usage:
+
+> char 'c' `shouldFail` "d"
+
+This is a convenience function wrapping 'shouldFailG'.
+-}
+shouldFailFor ::
+    (Show a, HasCallStack) =>
+    -- | Parser to run
+    Parsec e p a ->
+    -- | The input to run the parser on
+    p ->
+    Assertion
+p `shouldFailFor` i = parse p "" `shouldFailOn` i
+
+{- | Create Assertion to check that a parser fails to parse the given input.
+
+This is a more general version of 'shouldFailFor' that expects a parser
+from a stream to either an error or parsing result (instead of a 'Parsec') as its first input.
+
+Usage:
+
+> parse (char 'c') "" `shouldFailOn` "d"
+-}
+shouldFailOn ::
+    (Show a, HasCallStack) =>
+    -- | The parser to run
+    (t -> Either (ParseErrorBundle s e) a) ->
+    -- | The input to run the parser on
+    t ->
+    Assertion
+p `shouldFailOn` i = expectFailure $ p i
+
+{- | Check that a parser succeeds and leaves certain part of input
+unconsumed. Use it with functions like 'runParser'' and 'runParserT''
+that support incremental parsing.
+
+Usage:
+
+> runParser' (many (char 'x')) (initialState "xxa")
+>   `succeedsLeaving` "a"
+
+See also: 'initialState'.
+-}
+succeedsLeaving ::
+    ( HasCallStack
+    , Eq s
+    , ShowErrorComponent e
+    , VisualStream s
+    , TraversableStream s
+    , Show s
+    ) =>
+    -- | Parser that takes stream and produces result along with actual
+    -- state information
+    (State s e, Either (ParseErrorBundle s e) a) ->
+    -- | Part of input that should be left unconsumed
+    s ->
+    Assertion
+(st, r) `succeedsLeaving` s = do
+    expectSuccess_ r
+    checkUnconsumed s (stateInput st)
+
+{- | Check that a parser succeeds and leaves certain part of input
+unconsumed.
+
+This is a convenience function wrapping `succeedsLeaving'.
+
+Usage:
+
+> succeedsLeaving' (many (char 'x')) "xxa" "a"
+
+See also: 'initialState'.
+-}
+succeedsLeaving' ::
+    ( ShowErrorComponent e
+    , VisualStream s
+    , TraversableStream s
+    , Eq s
+    , Show s
+    ) =>
+    -- | Parser to run
+    Parsec e s a ->
+    -- | Input to the parser
+    s ->
+    -- | Part of the unput that should be left unconsumed
+    s ->
+    Assertion
+succeedsLeaving' p i r = runParser' p (initialState i) `succeedsLeaving` r
+
+{- | A combination of `shouldParse'` and `succeedsLeaving'`.
+
+Usage:
+
+> shouldParseLeaving (runParser' (some (char 'x'))
+>       (initialState "xkxa")) "xx" "b"
+-}
+shouldParseLeaving ::
+    ( VisualStream s
+    , TraversableStream s
+    , ShowErrorComponent e
+    , Show a
+    , Show s
+    , Eq a
+    , Eq s
+    ) =>
+    -- | Parser that takes stream and produces result along with actual
+    -- state information
+    (State s e, Either (ParseErrorBundle s e) a) ->
+    -- | The expected parsing result
+    a ->
+    -- | Part of input that should be left unconsumed
+    s ->
+    Assertion
+shouldParseLeaving (st, res) v r = do
+    resVal <- expectSuccess res
+    v @=? resVal
+    checkUnconsumed r (stateInput st)
+
+{- | A combination of `shouldParse'` and `succeedsLeaving'`.
+
+This is a convenience function wrapping `shouldParseLeaving`.
+
+Usage:
+
+> shouldParseLeaving' (some (char 'x')) "xkxa" "xx" "a"
+-}
+shouldParseLeaving' ::
+    ( VisualStream s
+    , TraversableStream s
+    , ShowErrorComponent e
+    , Show a
+    , Show s
+    , Eq a
+    , Eq s
+    ) =>
+    -- | Parser to run
+    Parsec e s a ->
+    -- Input to run the parser on
+    s ->
+    -- | The expected parsing result
+    a ->
+    -- | Part of the unput that should be left unconsumed
+    s ->
+    Assertion
+shouldParseLeaving' p i = shouldParseLeaving (runParser' p (initialState i))
+
+{- | Check that a parser fails and leaves a certain part of input
+unconsumed. Use it with functions like 'runParser'' and 'runParserT''
+that support incremental parsing.
+
+> runParser' (many (char 'x') <* eof) (initialState "xxa")
+>   `failsLeaving` "a"
+
+See also: 'initialState'.
+-}
+failsLeaving ::
+    ( HasCallStack
+    , Show a
+    , Eq s
+    , Show s
+    ) =>
+    -- | Parser that takes stream and produces result along with actual
+    -- state information
+    (State s e, Either (ParseErrorBundle s e) a) ->
+    -- | Part of input that should be left unconsumed
+    s ->
+    Assertion
+(st, r) `failsLeaving` s = do
+    expectFailure r
+    checkUnconsumed s (stateInput st)
+
+{- | Check that a parser fails and leaves a certain part of input
+unconsumed.
+
+This is a convenience function wrapping `succeedsLeaving'.
+
+Usage:
+
+> failsLeaving' (many (char 'x') <* eof) "xxa" "a"
+
+See also: 'initialState'.
+-}
+failsLeaving' ::
+    (Eq s, Show a, Show s) =>
+    -- | Parser to run
+    Parsec e s a ->
+    s ->
+    s ->
+    Assertion
+failsLeaving' p i r = runParser' p (initialState i) `failsLeaving` r
+
+----------------------------------------------------------------------------
+-- Helpers
+
+-- | Given input for parsing, construct initial state for parser.
+initialState :: s -> State s e
+initialState s =
+    State
+        { stateInput = s
+        , stateOffset = 0
+        , statePosState = initialPosState s
+        , stateParseErrors = []
+        }
+
+-- | Given input for parsing, construct initial positional state.
+initialPosState :: s -> PosState s
+initialPosState s =
+    PosState
+        { pstateInput = s
+        , pstateOffset = 0
+        , pstateSourcePos = initialPos ""
+        , pstateTabWidth = defaultTabWidth
+        , pstateLinePrefix = ""
+        }
+
+{- | Checks a parsing result for success.
+
+If the parsing was not unexpectedly unsuccessfull, the error produced by Megaparsec is printed.
+-}
+expectSuccess ::
+    (VisualStream s, TraversableStream s, ShowErrorComponent e, HasCallStack) =>
+    -- | The result of a run parser
+    Either (ParseErrorBundle s e) a ->
+    IO a
+expectSuccess r = case r of
+    Left e ->
+        assertFailure $
+            "the parser is expected to succeed, but it failed with:\n"
+                ++ showBundle e
+    Right v -> return v
+
+expectSuccess_ ::
+    (VisualStream s, TraversableStream s, ShowErrorComponent e, HasCallStack) =>
+    -- | The result of a run parser
+    Either (ParseErrorBundle s e) a ->
+    Assertion
+expectSuccess_ r = void $ expectSuccess r
+
+{- | Checks a parsing result for failure.
+
+If the parsing was unexpectedly successfull, the parsed result is printed.
+-}
+expectFailure ::
+    (Show a, HasCallStack) =>
+    -- The result of a run parser
+    Either (ParseErrorBundle s e) a ->
+    Assertion
+expectFailure r = case r of
+    Left _ -> return ()
+    Right v ->
+        assertFailure $
+            "the parser is expected to fail, but it parsed: " ++ show v
+
+-- | Compare two streams for equality and in the case of mismatch report it.
+checkUnconsumed ::
+    (Eq p, Show p, HasCallStack) =>
+    -- | Expected unconsumed output
+    p ->
+    -- | Actual unconsumed output
+    p ->
+    IO ()
+checkUnconsumed e a =
+    unless (e == a) . assertFailure $
+        "the parser is expected to leave unconsumed input: "
+            ++ show e
+            ++ "\nbut it left this: "
+            ++ show a
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,163 @@
+import Control.Monad.Combinators
+import Data.Void
+import Test.Tasty
+import Test.Tasty.ExpectedFailure
+import Test.Tasty.HUnit
+import Test.Tasty.Megaparsec
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  defaultMain $    testGroup
+        "Megaparsec Tasty Wrapper Tests"
+        [ shouldSucceedForTests
+        , failsLeavingTests
+        , succeedsLeavingTests
+        , shouldFailTests
+        , shouldParseTests
+        , parseSatisfiesTests
+        , shouldParseLeavingTests
+        ]
+
+
+
+
+{- | Convenience type for the parser used for testing
+
+Currently, we only test streams of type 'String'
+-}
+type Parser = Parsec Void String
+
+lC, aC :: Parser Char
+
+{- | Alias for the 'letterChar' parser
+
+This definition simply ensures that Haskell uses the correct type
+-}
+lC = letterChar :: Parser Char
+
+{- | Alias for the 'alphaNumChar' parser
+
+This definition simply ensures that Haskell uses the correct type
+-}
+aC = alphaNumChar :: Parser Char
+
+succeedsLeavingTests :: TestTree
+succeedsLeavingTests = testCase "Tests for `succeedsLeaving`" $ do
+    runParser' (many (char 'x') :: Parser String) (initialState "xxa") `succeedsLeaving` "a"
+    succeedsLeaving' (many (char 'x') :: Parser String) "xxa" "a"
+
+failsLeavingTests :: TestTree
+failsLeavingTests = testCase "Tests for `succeedsLeaving`" $ do
+    runParser' (many (char 'x' :: Parser Char) <* eof) (initialState "xxa") `failsLeaving` "a"
+    failsLeaving' (many (char 'x' :: Parser Char) <* eof) "xxa" "a"
+
+shouldParseLeavingTests :: TestTree
+shouldParseLeavingTests = testGroup "Tests for shouldParseLeaving" [succeeding, failing]
+  where
+    succeeding = testCase "succeeding tests" $ do
+        shouldParseLeaving' p "xxa" "xx" "a"
+        shouldParseLeaving (runParser' p (initialState "xxa")) "xx" "a"
+    failing =
+        testGroup
+            "expectedly failing tests"
+            [ expectFail $ testCase "fails on the comparison" $ do
+                shouldParseLeaving' (some (char 'x' :: Parser Char)) "xkxa" "xx" "a"
+                shouldParseLeaving (runParser' p (initialState "xkxa")) "xx" "a"
+            , expectFail $ testCase "fails on the rest" $ do
+                shouldParseLeaving' (some (char 'x' :: Parser Char)) "xxa" "xx" "b"
+                shouldParseLeaving (runParser' p (initialState "xkxa")) "xx" "b"
+            ]
+    p = (some (char 'x' :: Parser Char))
+
+shouldSucceedForTests :: TestTree
+shouldSucceedForTests =
+    testGroup
+        "Tests for `shouldSucceedFor`"
+        [ testCase "letterChar" $ do
+            lC `shouldSucceedFor` "x"
+            parse lC "" `shouldSucceedOn` "x"
+            lC `shouldSucceedFor` "xdg"
+        , testCase "some alphaNumChars" $ do
+            some aC `shouldSucceedFor` "xk43g"
+            some aC `shouldSucceedFor` "x"
+            parse (some aC) "" `shouldSucceedOn` "xk43g"
+        , testCase "many alphaNumChars" $ do
+            many aC `shouldSucceedFor` "xk43g"
+            many aC `shouldSucceedFor` "x"
+            many aC `shouldSucceedFor` ""
+            parse (many aC) "" `shouldSucceedOn` "x"
+        ]
+
+parseSatisfiesTests :: TestTree
+parseSatisfiesTests =
+    testGroup
+        "Tests for `parseSatisfies"
+        [ testCase "length of parsed alphaNumChars" $ do parse (many aC) "" "abc!nothere!" `parseSatisfies` ((== 3) . length)
+        , testCase "empty parse" $ do parse (many aC) "" "!notevenhere!abc!nothere!" `parseSatisfies` null
+        , testCase "length of parsed alphaNumChars" $ do parseSatisfies' (many aC) "abc!nothere!" ((== 3) . length)
+        , testCase "empty parse" $ do parseSatisfies' (many aC) "!notevenhere!abc!nothere!" null
+        ]
+
+shouldFailTests :: TestTree
+shouldFailTests =
+    testGroup
+        "Tests for `shouldFail`"
+        [ testCase "letterChar" $ do
+            lC `shouldFailFor` ""
+            lC `shouldFailFor` "3"
+            parse lC "" `shouldFailOn` ""
+            parse lC "" `shouldFailOn` "3"
+        , testCase "some letterChars" $ do
+            some lC `shouldFailFor` "43gdfhdf"
+            some lC `shouldFailFor` ""
+            parse (some lC) "" `shouldFailOn` "43gdfhdf"
+            parse (some lC) "" `shouldFailOn` ""
+        ]
+
+shouldParseTests :: TestTree
+shouldParseTests = testGroup "Tests for shouldParse" [original, convenient]
+  where
+    convenient =
+        testGroup
+            "Tests for convenient wrapper function"
+            [ testCase "letterChar" $ do
+                shouldParse' lC "x" 'x'
+                shouldParse' lC "xdg" 'x'
+            , testCase "some alphaNumChars" $ do
+                shouldParse' (some aC) "xk43g" "xk43g"
+                shouldParse' (some aC) "x" "x"
+            , testCase "many alphaNumChars" $ do
+                shouldParse' (many aC) "xk43g" "xk43g"
+                shouldParse' (many aC) "x" "x"
+                shouldParse' (many aC) "" ""
+            , testCase "some letterChars" $ do
+                shouldParse' (some lC) "xk43g" "xk"
+                shouldParse' (some lC) "x" "x"
+            , testCase "many letterChars" $ do
+                shouldParse' (many lC) "xk43g" "xk"
+                shouldParse' (many lC) "x" "x"
+                shouldParse' (many lC) "" ""
+            ]
+    original =
+        testGroup
+            "Tests for convenient wrapper function"
+            [ testCase "letterChar" $ do
+                parse lC "" "x" `shouldParse` 'x'
+                parse lC "" "xdg" `shouldParse` 'x'
+            , testCase "some alphaNumChars" $ do
+                parse (some aC) "" "xk43g" `shouldParse` "xk43g"
+                parse (some aC) "" "x" `shouldParse` "x"
+            , testCase "many alphaNumChars" $ do
+                parse (many aC) "" "xk43g" `shouldParse` "xk43g"
+                parse (many aC) "" "x" `shouldParse` "x"
+                parse (many aC) "" "" `shouldParse` ""
+            , testCase "some letterChars" $ do
+                parse (some lC) "" "xk43g" `shouldParse` "xk"
+                parse (some lC) "" "x" `shouldParse` "x"
+            , testCase "many letterChars" $ do
+                parse (many lC) "" "xk43g" `shouldParse` "xk"
+                parse (many lC) "" "x" `shouldParse` "x"
+                parse (many lC) "" "" `shouldParse` ""
+            ]
